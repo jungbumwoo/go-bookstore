6376c02c1e860f2789949e0060d95908a9007ea1
Jungbum Woo <jungbumwoo@naver.com>
2022-10-23 13:29:56 +0900

    add mysql, docker

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,15 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "jungbumwoo:matthewwoo12!/simplerest?charset=urf8")
+	DBMS := "mysql"
+	USER := "go_test"
+	PASS := "password"
+	PROTOCOL := "tcp(go-boostoreDB)"
+	DBNAME := "go_database"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	d, err := gorm.Open(DBMS, CONNECT)
 	if err != nil {
 		panic(err)
 	}
