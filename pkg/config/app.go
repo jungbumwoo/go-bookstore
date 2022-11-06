72501c1673d06169c24de09214d47d5f20d08362
Matt <matthew.woo@buzzvil.com>
2022-11-06 14:37:05 +0900

    :construction: wip

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
 )
 
 var (
@@ -13,13 +14,14 @@ func Connect() {
 	DBMS := "mysql"
 	USER := "go_test"
 	PASS := "password"
-	PROTOCOL := "tcp(127.0.0.1)"
+	PROTOCOL := "tcp(127.0.0.2)"
 	DBNAME := "go_database"
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 
 	d, err := gorm.Open(DBMS, CONNECT)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	db = d
